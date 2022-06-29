c535245a609a5ad3bc8fd5d5e83993d3c7dcd031
kumastry <chma19063@g.nihon-u.ac.jp>
2022-06-29 12:59:24 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import * as d3 from "d3";
+import { useEffect, useState,useRef } from "react";
+
+
+function App() {
+
+    useEffect(() => {
+        
+    }, [])
+
+
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+  
+  export default App;
